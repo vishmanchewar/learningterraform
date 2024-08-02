6bfa3873eb1f7ec4a2ef9aa5df4e54787fc5160a
vishmanchewar <vish.manchewar7@gmail.com>
2024-08-02 16:10:01 +0530

    added terraform file to create security group

---
+++ first-terraform-resource/aws/createSecurityGroup.tf
@@ -0,0 +1,15 @@
+# AWS Provider to work with ap-south-1 region.
+provider "aws" {
+  region = "ap-south-1"
+  
+}
+
+module "web_server_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+
+  name        = "web-server"
+  description = "Security group for web-server with HTTP ports open within VPC"
+  vpc_id      = "vpc-12345678"
+
+  ingress_cidr_blocks = ["10.10.0.0/16"]
+}
